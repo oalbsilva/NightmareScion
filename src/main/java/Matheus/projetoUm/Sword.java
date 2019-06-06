1fc09ab97791528df9fbbb34c533e24d0de202d9
Matheus-Mikatral <matheus_santana92@hotmail.com>
2019-06-05 21:51:39 -0300

    Removendo comentarios desnecessarios

--- src/main/java/Matheus/projetoUm/Sword.java
+++ src/main/java/Matheus/projetoUm/Sword.java
@@ -1,28 +1,28 @@
 package Matheus.projetoUm;
 
-public class Sword extends Item implements ArmaDeMao{
-	
+public class Sword extends Item implements ArmaDeMao {
+
 	protected void name() {
-		
+
 	}
 
 	public boolean podeSerUsadoPorPaladino() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	public boolean podeSerUsadoPorSorcerer() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	public boolean podeSerUsadoPorKnight() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	public boolean podeSerUsadoPorDruid() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
